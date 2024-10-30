2ab0f924a29738df3aa5391f05511c2c91717a5d
alireza <b.alireza1390@gmail.com>
2024-10-30 18:05:51 +0330

    first save

---
+++ src/js/script.js
@@ -0,0 +1,148 @@
+//sticky navBar
+window.addEventListener("scroll", stickyNav);
+window.addEventListener("resize", stickyNav);
+window.addEventListener("load", stickyNav);
+
+const cont = document.querySelector(".cont-nav");
+
+function stickyNav() {
+  if (window.innerWidth < 1024) {
+    cont.classList.remove("sticky");
+  } else {
+    const header = document.querySelector(".nav-bar");
+    const rect = header.getBoundingClientRect();
+    const headerBottom = rect.bottom + rect.height;
+    if (headerBottom <= window.scrollY) {
+      cont.classList.add("sticky");
+    } else {
+      cont.classList.remove("sticky");
+    }
+  }
+}
+//sticky navBar
+
+//search
+const searchBtn = document.querySelector('.search-box');
+const searchSection = document.querySelector('.search');
+const searchLayer = document.querySelector('.search-layer')
+searchBtn.addEventListener('click',()=>{
+searchSection.classList.remove('scale-y-0');
+})
+searchLayer.addEventListener('click',()=>{
+  searchSection.classList.add('scale-y-0');
+})
+
+
+console.log(searchBtn)
+//search
+
+//mobile menu
+const trigger = document.querySelector(".nav-trigger");
+const navOverlay = document.querySelector(".mobile-overlay");
+const close = document.querySelector(".mobile-menu .close");
+const mobileMenu = document.querySelector(".mobile-menu");
+console.log(navOverlay);
+trigger.addEventListener("click", () => {
+openMenu();
+});
+navOverlay.addEventListener("click", () => {
+  closeMenu();
+});
+close.addEventListener("click", () => {
+closeMenu();
+});
+
+function closeMenu(){
+  navOverlay.classList.toggle("delay-500");
+  mobileMenu.classList.toggle("delay-500");
+  navOverlay.classList.add("translate-x-full");
+  mobileMenu.classList.add("translate-x-full");
+}
+function openMenu(){
+  navOverlay.classList.toggle("delay-500");
+  mobileMenu.classList.toggle("delay-500");
+  navOverlay.classList.remove("translate-x-full");
+  mobileMenu.classList.remove("translate-x-full");
+}
+
+//mobile menu
+
+//slider
+
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const sliderWrap=document.querySelector('.slider-wrapper');
+const slide=document.querySelectorAll('.slider-wrapper .slide');
+responsive = [
+  { breakpoint: { width: 576, item: 2 } },
+  { breakpoint: { width: 1024, item: 4 } },
+  { breakpoint: { width: 1280, item: 5 } }
+]
+let index = 0;
+
+
+
+next.addEventListener("click", () => {
+  nextSlide();
+});
+prev.addEventListener("click", () => {
+  prevSlide();
+
+});
+window.addEventListener("resize", ()=>{
+sliderWrap.style.transform=`translateX(0%)`;
+index=0;
+reponseItems();
+offSlide();
+
+});
+function reponseItems(){
+for (let i = 0; i < responsive.length; i++) {
+  if (window.innerWidth > responsive[i].breakpoint.width) {
+      items = responsive[i].breakpoint.item;
+      console.log(items)
+  }
+}
+}
+
+
+function nextSlide(){
+reponseItems();
+  if(index<(slide.length-items)){
+  index++;
+  let move =(100/items)*index;
+sliderWrap.style.transform=`translateX(${move}%)`;
+offSlide();
+
+  }
+}
+
+function prevSlide(){
+  reponseItems();
+  if(index>=1){
+  index--;
+  let move =(100/items)*index;
+sliderWrap.style.transform=`translateX(${move}%)`;
+offSlide();
+  }
+}
+function offSlide(){
+  if(index<1){
+    prev.style.opacity=0.2;
+    prev.style.pointerEvents='none';
+  }else{
+    prev.style.opacity=1;
+    prev.style.pointerEvents='all';
+  }
+  if(index==(slide.length-items)){
+    next.style.opacity=0.2;
+    next.style.pointerEvents='none';
+  }else{
+    next.style.opacity=1;
+    next.style.pointerEvents='all';
+  }
+}
+offSlide();
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,145 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/*.{html,js}"],
+    // content["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            white: '#fff',
+            blue: {
+                100: '#62ddff',
+                150:'#1239ac',
+                200: '#1439ab',
+                300: '#092e99',
+                400: '#0a2d95'
+                
+            },
+            Turquoise: '#62ddff',
+            darkBlue: '#0a2d95',
+            orange: '#ffb400',
+            gray:{ 
+                100:'#d7d6dc', 
+                200:'#5b5e6a'
+
+            },
+            // fontFamily: {
+         
+            //     "jmhuri":"jmhuri",
+             
+            // },
+            // fontFamily: {
+            //     "vazir":"vazir"
+            // },
+            // fontFamily: {
+            //     "heshmat": "heshmat",
+            // },
+
+
+            screens: {
+                'sm': '576px',
+                // => @media (min-width: 640px) { ... }
+          
+                'md': '768px',
+                // => @media (min-width: 768px) { ... }
+          
+                'lg': '1024px',
+                // => @media (min-width: 1024px) { ... }
+          
+                'xl': '1280px',
+                // => @media (min-width: 1280px) { ... }
+          
+              }
+
+        },
+        extend: {
+            boxShadow: {
+                'normal': '0px 10px 60px 0px rgba(0, 0, 0, 0.1)',
+                'slide':'0px 5px 3px 0px rgba(0,0,0,.4)',
+              },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            animation:{
+                title:'title 1.5s ease-in-out',
+                   zoominout:'zoominout 3s ease-in-out infinite',
+                   zoominout2:'zoominout2 3s ease-in-out infinite',
+                   zoom:'zoom .8s ease-in-out forwards',
+                   zoomFade:'zoomFade 5s linear infinite',
+            },
+
+
+
+            keyframes:{
+                title:{
+                    '0%':{opacity:'0',
+                        transform: 'translateY(100px)',
+                    },
+                    '100%':{opacity:'1',
+                    transform: 'translateY(0px)',
+                    }
+                },
+      
+                zoominout:{
+                    '0%':{ transform:'rotate(0deg) scale(0.5)',
+                        opacity:'0',
+                    },
+                    '50%':{ transform:'rotate(180deg) scale(.8)',
+                        opacity:'.3',
+                    },
+                    '100%':{ transform:'rotate(360deg) scale(0.5)',
+                        opacity:'0',
+                    },
+          
+
+                },
+                zoominout2:{
+                    '0%':{ transform:'rotate(0deg) scale(0.5)',
+                        opacity:'0',
+                    },
+                    '50%':{ transform:'rotate(0deg) scale(.8)',
+                        opacity:'.3',
+                    },
+                    '100%':{ transform:'rotate(0deg) scale(0.5)',
+                        opacity:'0',
+                    },
+                },
+                    zoom:{
+                        '0%':{
+                            transform:'scale(1)',
+                        },
+                        '100%':{
+                            transform:'scale(1.02)',
+                        }
+                    },
+                    zoomFade:{
+                        '0%':{
+                            transform:'translateX(40px) translateY(0) scale(0.9)',
+                        },
+                        '50%':{
+                            transform:'translateX(-40px) translateY(2%) scale(1)',
+                           
+                        },
+                        '100%':{
+                            transform:'translateX(40px) translateY(0) scale(0.9)',
+                        },
+                    },
+          
+
+                
+            
+            },
+
+        },
+    },
+    plugins: [],
+}
